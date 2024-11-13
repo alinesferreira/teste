05e4e902364aa99961a077cf7094dca6cbc8fcea
alinesferreira <67583671+alinesferreira@users.noreply.github.com>
2024-11-13 11:25:18 -0300

    Update questao_2.py
    retirada questão 3

--- questao_2.py
+++ questao_2.py
@@ -9,13 +9,34 @@ st.caption("Esta é a a legenda")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
-# Questao 3
-# Utilizando select_slider para obter o grau de satisfação do cliente (0 a 100)
-satisfacao = st.select_slider(
-    'Qual é o seu nível de satisfação?',
-    options=range(0, 101),
-    value=50  # Valor padrão
-)
 
-# Exibindo o resultado selecionado
-st.write(f"Sua avaliação de satisfação é: {satisfacao}/100")
+
+
+# Questao 4
+
+# criando elementos gráficos
+'''Informar como colher os dados através de variáveis'''
+x = st.checkbox('Sim')
+st.write(x)
+
+# fazer o mesmo com alguns dos comandos abaixo
+st.title(x)
+st.button('Clique')
+st.radio('Selecione seu gênero',['Masculino','Feminino'])
+st.selectbox('Selecione seu gênero',['Masculino','Feminino'])
+st.multiselect('Escolha um departamento',['DCS', 'DE', 'DIR'])
+st.select_slider('Selecione uma resposta', ['Ruim', 'Bom', 'Excelente'])
+st.slider('Selecione um número', 0,50)
+st.number_input('Selecione um número', 0,10)
+st.text_input('Endereço de e-mail')
+st.date_input('Data de viagem')
+st.time_input('Tempo de escola')
+st.text_area('Descrição')
+st.file_uploader('Atualize uma foto')
+st.color_picker('Escolha sua cor favorita')
+
+# mensagens de status
+st.success("Você conseguiu!")
+st.error("Erro!")
+st.warning("Advertência")
+st.info("Esta é uma informação")

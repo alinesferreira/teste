efa61f7d2eee48a860fc7a31ac7526299a2b6dfd
alinesferreira <67583671+alinesferreira@users.noreply.github.com>
2024-11-13 11:23:20 -0300

    Update questao_2.py
    acrescentei a questão 3

--- questao_2.py
+++ questao_2.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
+#Questao 2
 st.title("Este é o título do app")
 st.header("Este é o subtítulo")
 st.subheader("Este é o terceiro subtítulo")
@@ -6,3 +8,14 @@ st.markdown("Este é texto")
 st.caption("Esta é a a legenda")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+# Questao 3
+# Utilizando select_slider para obter o grau de satisfação do cliente (0 a 100)
+satisfacao = st.select_slider(
+    'Qual é o seu nível de satisfação?',
+    options=range(0, 101),
+    value=50  # Valor padrão
+)
+
+# Exibindo o resultado selecionado
+st.write(f"Sua avaliação de satisfação é: {satisfacao}/100")
